c3fbea94313ec67ab8339efcdef80da8cb3edf0a
Videl90 <lorena.sa.0117@gmail.com>
2020-05-05 16:44:35 -0500

    modified the loop por changing colors

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 var saveButton = $(".save-button");
 
 
+
+
 //This changes the rows color
 
-$(".row1").attr("data-time", moment("8:00 am", "h:mm a").format("HH"));
-$(".row2").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
-$(".row3").attr("data-time", moment("10:00 am", "hh:mm a").format("HH"));
-$(".row4").attr("data-time", moment("11:00 am", "hh:mm a").format("HH"));
-$(".row5").attr("data-time", moment("12:00 pm", "hh:mm a").format("HH"));
-$(".row6").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
-$(".row7").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
-$(".row8").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
-$(".row9").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
-$(".row10").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
-$(".row11").attr("data-time", moment("6:00 pm", "h:mm a").format("HH"));
+$("#8Task").attr("data-index", moment("8:00 am", "h:mm a").format("HH"));
+$("#9Task").attr("data-index", moment("9:00 am", "h:mm a").format("HH"));
+$("#10Task").attr("data-index", moment("10:00 am", "hh:mm a").format("HH"));
+$("#11Task").attr("data-index", moment("11:00 am", "hh:mm a").format("HH"));
+$("#12Task").attr("data-index", moment("12:00 pm", "hh:mm a").format("HH"));
+$("#13Task").attr("data-index", moment("1:00 pm", "h:mm a").format("HH"));
+$("#14Task").attr("data-index", moment("2:00 pm", "h:mm a").format("HH"));
+$("#15Task").attr("data-index", moment("3:00 pm", "h:mm a").format("HH"));
+$("#16Task").attr("data-index", moment("4:00 pm", "h:mm a").format("HH"));
+$("#17Task").attr("data-index", moment("5:00 pm", "h:mm a").format("HH"));
+$("#18Task").attr("data-index", moment("6:00 pm", "h:mm a").format("HH"));
 
 
 
@@ -28,28 +30,38 @@ $(document).ready(function() {
 
     console.log(currentDay);
 
-    //function and variables to change color
-   
-    var currentHour = parseInt(moment().format('H'));
-    var chooseHour = parseInt($('#hour').text());
-    
-    console.log(currentHour);
-    console.log(chooseHour);
+    //function and variables to change color. This whill do a loop through all the elements inside the tr
 
-    if (chooseHour === currentHour){
-        $(".form-control").addClass('present');
-    } else if (chooseHour < currentHour){
-        $(".form-control").addClass('past');
-    } else {
-        $(".form-control").addClass('future');
-    }
+    $("tr").each(function(i) {
+        var currentHour = parseInt(moment().format('H'));
+        console.log(currentHour);
+        var chooseHour = parseInt($(this).find(".save-button").attr("data-index"));
+        
+        console.log(currentHour);
+        console.log(chooseHour);
+
+        if (chooseHour === currentHour){
+            $(this).find('.form-control').addClass('present');
+        } else if (chooseHour < currentHour){
+            $(this).find('.form-control').addClass('past');
+        } else {
+            $(this).find('.form-control').addClass('future');
+        }
+    })
+    
+   
+//}
+
+    
 
 
     //Variables to create the local storage/
+
+    var tasks = ["#task1", ]
   
     saveButton.on("click", function(){
         var hr = $(this).attr("data-index");
-        var task = $("#task").val();
+        var task = $("task").val();
         localStorage.setItem(hr, task);
 
     })
